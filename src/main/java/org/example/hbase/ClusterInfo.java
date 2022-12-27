763b4e2944c357d8d9cba86ac68b88ac8927f956
phuld <phuledinh@channelvn.net>
2022-12-27 22:09:12 +0700

    update

--- src/main/java/org/example/hbase/ClusterInfo.java
+++ src/main/java/org/example/hbase/ClusterInfo.java
@@ -29,56 +29,61 @@ public class ClusterInfo {
 
         ClusterStatus clusterStatus = admin.getClusterStatus();
         Map<ServerName, ServerMetrics> serverMetricsMap = clusterStatus.getLiveServerMetrics();
-
-
-        Collection<ServerName> servers = admin.getRegionServers();
-        RegionMetrics regionMaxReq = null; // region có lượng read request nhiều nhất
-
-        ServerName sourceMove = null; // server chứa region ở trên
-        ServerName destinationMove = null; // server có lượng i/o it nhất
-
-        long maxRegionReadReq = 0;
-        long minServerReq = Long.MAX_VALUE;
-
-        for (ServerName serverName : servers) {
-            List<RegionMetrics> regionMetricsList = admin.getRegionMetrics(serverName, table);
-            long totlaReq = 0; // tổng request của server đó
-
-            for (RegionMetrics regionMetrics : regionMetricsList) {
-                long numReadReq = regionMetrics.getReadRequestCount();
-                long numWriteRed = regionMetrics.getWriteRequestCount();
-
-                totlaReq += numReadReq + numWriteRed;
-                if (numReadReq > maxRegionReadReq) {
-                    maxRegionReadReq = numReadReq;
-                    regionMaxReq = regionMetrics;
-                    sourceMove = serverName;
-                }
-            }
-
-            if (totlaReq < minServerReq) {
-                minServerReq = totlaReq;
-                destinationMove = serverName;
-            }
+        for (Map.Entry<ServerName, ServerMetrics> entry : serverMetricsMap.entrySet()) {
+            System.out.println("server name: " + entry.getKey());
+            ServerMetrics serverMetrics = entry.getValue();
+            System.out.println("request per second: " + serverMetrics.getRequestCountPerSecond());
         }
 
-        if (sourceMove != null && destinationMove != null) {
-            System.out.println("Result: ");
-            System.out.println("Region max: ");
-            System.out.println("region name: " + new String(regionMaxReq.getRegionName(), StandardCharsets.UTF_8));
-            System.out.println("num read request: " + regionMaxReq.getReadRequestCount());
-            System.out.println("num write request: " + regionMaxReq.getWriteRequestCount());
 
-            System.out.println("source server move: " + sourceMove);
-            System.out.println("destination server move: " + destinationMove);
-
-            long t1 = System.currentTimeMillis();
-
-            System.out.println(RegionInfo.encodeRegionName(regionMaxReq.getRegionName()));
-            admin.move(RegionInfo.encodeRegionName(regionMaxReq.getRegionName()).getBytes(), destinationMove);
-            long t2 = System.currentTimeMillis();
-            System.out.println("time move: " + (t2 - t1) + " ms");
-        }
+//        Collection<ServerName> servers = admin.getRegionServers();
+//        RegionMetrics regionMaxReq = null; // region có lượng read request nhiều nhất
+//
+//        ServerName sourceMove = null; // server chứa region ở trên
+//        ServerName destinationMove = null; // server có lượng i/o it nhất
+//
+//        long maxRegionReadReq = 0;
+//        long minServerReq = Long.MAX_VALUE;
+//
+//        for (ServerName serverName : servers) {
+//            List<RegionMetrics> regionMetricsList = admin.getRegionMetrics(serverName, table);
+//            long totlaReq = 0; // tổng request của server đó
+//
+//            for (RegionMetrics regionMetrics : regionMetricsList) {
+//                long numReadReq = regionMetrics.getReadRequestCount();
+//                long numWriteRed = regionMetrics.getWriteRequestCount();
+//
+//                totlaReq += numReadReq + numWriteRed;
+//                if (numReadReq > maxRegionReadReq) {
+//                    maxRegionReadReq = numReadReq;
+//                    regionMaxReq = regionMetrics;
+//                    sourceMove = serverName;
+//                }
+//            }
+//
+//            if (totlaReq < minServerReq) {
+//                minServerReq = totlaReq;
+//                destinationMove = serverName;
+//            }
+//        }
+//
+//        if (sourceMove != null && destinationMove != null) {
+//            System.out.println("Result: ");
+//            System.out.println("Region max: ");
+//            System.out.println("region name: " + new String(regionMaxReq.getRegionName(), StandardCharsets.UTF_8));
+//            System.out.println("num read request: " + regionMaxReq.getReadRequestCount());
+//            System.out.println("num write request: " + regionMaxReq.getWriteRequestCount());
+//
+//            System.out.println("source server move: " + sourceMove);
+//            System.out.println("destination server move: " + destinationMove);
+//
+//            long t1 = System.currentTimeMillis();
+//
+//            System.out.println(RegionInfo.encodeRegionName(regionMaxReq.getRegionName()));
+//            admin.move(RegionInfo.encodeRegionName(regionMaxReq.getRegionName()).getBytes(), destinationMove);
+//            long t2 = System.currentTimeMillis();
+//            System.out.println("time move: " + (t2 - t1) + " ms");
+//        }
 
         connection.close();
 
